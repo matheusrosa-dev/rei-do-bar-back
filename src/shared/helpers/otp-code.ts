d56983008b33374124e6cc9f50aa646e710ede21
Matheus Sena Rosa <matheussenarosa8@gmail.com>
2026-05-21 11:54:16 -0300

    fix(otp): update character set for OTP code generation to exclude similar characters

--- src/shared/helpers/otp-code.ts
+++ src/shared/helpers/otp-code.ts
@@ -2,7 +2,7 @@ import { hashString } from "./string";
 
 export function generateOtpCode() {
   const codeLength = 6;
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const chars = "ABCDEFGHIJKLMNPQRSTUVWXYZ123456789";
   const values = crypto.getRandomValues(new Uint8Array(codeLength));
 
   const code = Array.from(values, (value) => chars[value % chars.length]).join(
