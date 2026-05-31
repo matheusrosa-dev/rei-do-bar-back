231d8dbf6d0666e42493d39906e22170b24ef1a5
Matheus Sena Rosa <matheussenarosa8@gmail.com>
2026-05-31 16:59:10 -0300

    feat: import SettingsModule into orders and cart modules

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { CartService } from "./cart.service";
 import { CartController } from "./cart.controller";
+import { SettingsModule } from "@shared/settings/settings.module";
 
 @Module({
+  imports: [SettingsModule],
   controllers: [CartController],
   providers: [CartService],
 })
